9b6336ea118da00f9f35d0121df09398094de743
StefanoIncardone <stefanoincardone@gmail.com>
2023-04-27 17:24:24 +0200

    Improved syntax errors

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 // TODO create IDEAS.md
-use std::{io::{BufReader, BufRead, ErrorKind}, fs::File, env, process::ExitCode, fmt::{Display, write}};
+use std::{io::{BufReader, BufRead, ErrorKind}, fs::File, env, process::ExitCode, fmt::Display};
 
 
 #[derive( Debug, PartialEq )]
@@ -56,25 +56,26 @@ enum TokenKind {
     EOF,
 }
 
-impl Display for TokenKind {
-    fn fmt( &self, f: &mut std::fmt::Formatter<'_> ) -> std::fmt::Result {
-        match self {
-            Self::Unexpected( text, _ ) => write!( f, "{}", text ),
-            Self::OpenRoundBracket => write!( f, "(" ),
-            Self::CloseRoundBracket => write!( f, ")" ),
-            Self::Literal{ kind } => match kind {
-                LiteralKind::Int{ base: _, value } => write!( f, "{}", value ),
-            },
-            Self::Comment( text ) => write!( f, "{}", text ),
-            Self::Plus => write!( f, "+" ),
-            Self::Minus => write!( f, "-" ),
-            Self::Times => write!( f, "*" ),
-            Self::Divide => write!( f, "/" ),
-            Self::Pow => write!( f, "^" ),
-            Self::EOF => write!( f, "" ),
-        }
-    }
-}
+// impl Display for TokenKind {
+//     fn fmt( &self, f: &mut std::fmt::Formatter<'_> ) -> std::fmt::Result {
+//         match self {
+//             Self::Unexpected( text, _ ) => write!( f, "{}", text ),
+//             Self::OpenRoundBracket => write!( f, "(" ),
+//             Self::CloseRoundBracket => write!( f, ")" ),
+//             Self::Literal{ kind } => match kind {
+//                 LiteralKind::Int{ base: _, value } => write!( f, "{}", value ),
+//             },
+//             Self::Comment( text ) => write!( f, "{}", text ),
+//             Self::Plus => write!( f, "+" ),
+//             Self::Minus => write!( f, "-" ),
+//             Self::Times => write!( f, "*" ),
+//             Self::Divide => write!( f, "/" ),
+//             Self::Pow => write!( f, "^" ),
+//             // Self::Equals => write!( f, "=" ),
+//             Self::EOF => write!( f, "" ),
+//         }
+//     }
+// }
 
 #[derive( Debug )]
 struct Token {
@@ -88,30 +89,31 @@ struct Line {
     tokens: Vec<Token>,
 }
 
-impl Display for Line {
-    fn fmt( &self, f: &mut std::fmt::Formatter<'_> ) -> std::fmt::Result {
-        if self.tokens.len() == 0 {
-            write!( f, "" )?;
-            return Ok( () );
-        }
+// impl Display for Line {
+//     fn fmt( &self, f: &mut std::fmt::Formatter<'_> ) -> std::fmt::Result {
+//         if self.tokens.len() == 0 {
+//             write!( f, "" )?;
+//             return Ok( () );
+//         }
 
-        write!( f, "{}", " ".repeat( self.tokens[ 0 ].col - 1 ) )?;
+//         // leading whitespaces
+//         write!( f, "{}", " ".repeat( self.tokens[ 0 ].col - 1 ) )?;
 
-        let mut tokens = self.tokens.iter().peekable();
-        while let Some( token ) = tokens.next() {
-            let spaces = if let Some( next_token ) = tokens.peek() {
-                ((*next_token).col - token.col) - token.kind.to_string().len()
-            }
-            else {
-                0
-            };
+//         let mut tokens = self.tokens.iter().peekable();
+//         while let Some( token ) = tokens.next() {
+//             let spaces = if let Some( next_token ) = tokens.peek() {
+//                 ((*next_token).col - token.col) - token.kind.to_string().len()
+//             }
+//             else {
+//                 0
+//             };
 
-            write!( f, "{}{}", token.kind, " ".repeat( spaces ) )?;
-        }
+//             write!( f, "{}{}", token.kind, " ".repeat( spaces ) )?;
+//         }
 
-        return Ok( () );
-    }
-}
+//         return Ok( () );
+//     }
+// }
 
 #[derive( Debug )]
 struct Lexer {
@@ -126,11 +128,11 @@ impl Lexer {
         let mut row: usize = 1;
 
         for source_line in BufReader::new( source_file ).lines() {
-            if let Ok( l ) = source_line {
+            if let Ok( current_line ) = source_line {
                 let mut col: usize = 1;
                 let mut line = Line{ row, tokens: Vec::new() };
 
-                let mut src = l.chars().peekable();
+                let mut src = current_line.chars().peekable();
                 while let Some( ch ) = src.next() {
                     let token: Token = match ch {
                         // TODO handle other whitespace characters
@@ -215,6 +217,8 @@ impl Lexer {
                         _ => Token{ kind: TokenKind::Unexpected( ch.into(), "unrecognized token" ), col },
                     };
 
+                    // TODO push to a struct containing all errors and print that instead
+                    Self::print_error( &file_path, row, &current_line, &token );
                     line.tokens.push( token );
                     col += 1;
                 }
@@ -226,33 +230,23 @@ impl Lexer {
 
         lines.push( Line{ row, tokens: vec![ Token{ kind: TokenKind::EOF, col: 1 } ] } );
 
-        let lexer =  Self{ file_path, lines };
-        lexer.print_error_context();
-        return lexer;
+        return Self{ file_path, lines };
     }
 
-    fn print_error_context( &self ) {
-        let padding_count = self.lines.len().ilog10() as usize + 1;
-        let padding = " ".repeat( padding_count );
-        
-        for line in &self.lines {
-            let line_text = line.to_string();
-            for token in &line.tokens {
-                match &token.kind {
-                    TokenKind::Unexpected( token_text, err ) => {
-                        // TODO pad line number left so that the right bar is positioned base on the line number digit count
-                        let error_visualization = &format!(
-                            " {} |\n {: >padding_count$} | {}\n {} | {}{} <- {}",
-                            padding, line.row, line_text, padding,
-                            " ".repeat( token.col - 1 ), "^".repeat( token_text.len() ), err
-                        );
+    fn print_error( file_path: &str, row: usize, line: &str, token: &Token ) {
+        if let TokenKind::Unexpected( token_text, err ) = &token.kind  {
+            let padding_count = row.ilog10() as usize + 1;
+            let padding = " ".repeat( padding_count );
+            let bar = "\x1b[94m|\x1b[0m";
+            let error_visualization = &format!(
+                " {padding} {bar}\n \x1b[94m{: >padding_count$}\x1b[0m {bar} {}\n {padding} {bar} {}\x1b[91m{} <- {}\x1b[0m",
+                row, line, " ".repeat( token.col - 1 ), "^".repeat( token_text.len() ), err
+            );
 
-                        eprintln!( "Error:\n {padding} > {}:{}:{}\n{}\n", self.file_path, line.row, token.col, error_visualization );
-                    }
-                    _ => continue,
-                }
-
-            }
+            eprintln!(
+                "\x1b[91mError\x1b[0m:\n {padding} \x1b[94m>\x1b[0m {}:{}:{}\n{}\n",
+                file_path, row, token.col, error_visualization
+            );
         }
     }
 }
@@ -281,12 +275,15 @@ impl Lexer {
 // enum Node {
 //     Unrecognized,
 
-//     Variable{
-//         mutability: Mutability,
-//         name: Option<String>,
-//         typ: String,
-//         value: Option<Value>,
-//     },
+//     Literal( Value ),
+//     Node( Box<Node> )
+
+    // Variable{
+    //     mutability: Mutability,
+    //     name: Option<String>,
+    //     typ: String,
+    //     value: Option<Value>,
+    // },
 // }
 
 // #[derive( Debug )]
@@ -297,96 +294,96 @@ impl Lexer {
 // impl AST {
 //     fn parse( lexer: &Lexer ) -> Self {
 //         let mut statements: Vec<Node> = Vec::new();
-//         for line in &lexer.lines {
-//             let mut tokens = line.tokens.iter().peekable();
-//             while let Some( current_token ) = tokens.next() {
-//                 match current_token.kind {
-//                     TokenKind::Comment( _ ) /* | TokenKind::NewLine */ | TokenKind::EOF | TokenKind::SemiColon => continue,
-//                     TokenKind::Let | TokenKind::Var | TokenKind::Const => {
-//                         let mutability = match current_token.kind {
-//                             TokenKind::Let => Mutability::Let,
-//                             TokenKind::Var => Mutability::Var,
-//                             TokenKind::Const => Mutability::Const,
-//                             _ => unreachable!(),
-//                         };
+        // for line in &lexer.lines {
+        //     let mut tokens = line.tokens.iter().peekable();
+        //     while let Some( current_token ) = tokens.next() {
+        //         match current_token.kind {
+        //             TokenKind::Comment( _ ) /* | TokenKind::NewLine */ | TokenKind::EOF | TokenKind::SemiColon => continue,
+        //             TokenKind::Let | TokenKind::Var | TokenKind::Const => {
+        //                 let mutability = match current_token.kind {
+        //                     TokenKind::Let => Mutability::Let,
+        //                     TokenKind::Var => Mutability::Var,
+        //                     TokenKind::Const => Mutability::Const,
+        //                     _ => unreachable!(),
+        //                 };
 
-//                         let name = match tokens.next() {
-//                             Some( token ) => match &token.kind {
-//                                 TokenKind::Identifier( var_name ) => Some( var_name.clone() ),
-//                                 _ => {
-//                                     eprintln!( "Error: expected identifier in variable definition!" );
-//                                     None
-//                                 }
-//                             },
-//                             None => {
-//                                 eprintln!( "Error: Run out of tokens!" );
-//                                 None
-//                             },
-//                         };
-                        
-//                         let mut typ = match tokens.next() {
-//                             Some( token ) => match &token.kind {
-//                                 TokenKind::Colon => match tokens.next() {
-//                                     Some( var_type ) => match &var_type.kind {
-//                                         TokenKind::Identifier( type_name ) => type_name.clone(),
-//                                         _ => {
-//                                             eprintln!( "Error: expected type identifier!" );
-//                                             String::new()
-//                                         }
-//                                     }
-//                                     None => {
-//                                         eprintln!( "Error: run out of tokens!" );
-//                                         String::new()
-//                                     }
-//                                 }
-//                                 _ => {
-//                                     eprintln!( "Error: expected type qualification!" );
-//                                     String::new()
-//                                 }
-//                             },
-//                             None => {
-//                                 eprintln!( "Error: run out of tokens!" );
-//                                 String::new()
-//                             }
-//                         };
-                        
-//                         let value = match tokens.next() {
-//                             Some( token ) => match &token.kind {
-//                                 TokenKind::Equals => match tokens.next() {
-//                                     Some( initializer ) => match &initializer.kind {
-//                                         TokenKind::Literal{ kind } => match kind {
-//                                             LiteralKind::Int{ base, value } => {
-//                                                 typ = "int".to_string();
-//                                                 Some( Value::Integer( value.clone(), base.clone() ) )
-//                                             }
-//                                         },
-//                                         _ => {
-//                                             eprintln!( "Error: expected literal or expression!" );
-//                                             None
-//                                         }
-//                                     }
-//                                     None => {
-//                                         eprintln!( "Error: run out of tokens!" );
-//                                         None
-//                                     },
-//                                 },
-//                                 _ => {
-//                                     eprintln!( "Error: expected equals" );
-//                                     None
-//                                 },
-//                             },
-//                             None => {
-//                                 eprintln!( "Error: run out of tokens!" );
-//                                 None
-//                             },
-//                         };
+        //                 let name = match tokens.next() {
+        //                     Some( token ) => match &token.kind {
+        //                         TokenKind::Identifier( var_name ) => Some( var_name.clone() ),
+        //                         _ => {
+        //                             eprintln!( "Error: expected identifier in variable definition!" );
+        //                             None
+        //                         }
+        //                     },
+        //                     None => {
+        //                         eprintln!( "Error: Run out of tokens!" );
+        //                         None
+        //                     },
+        //                 };
 
-//                         statements.push( Node::Variable{ mutability, typ, name, value } );
-//                     }
-//                     _ => unreachable!(),
-//                 }
-//             }
-//         }
+        //                 let mut typ = match tokens.next() {
+        //                     Some( token ) => match &token.kind {
+        //                         TokenKind::Colon => match tokens.next() {
+        //                             Some( var_type ) => match &var_type.kind {
+        //                                 TokenKind::Identifier( type_name ) => type_name.clone(),
+        //                                 _ => {
+        //                                     eprintln!( "Error: expected type identifier!" );
+        //                                     String::new()
+        //                                 }
+        //                             }
+        //                             None => {
+        //                                 eprintln!( "Error: run out of tokens!" );
+        //                                 String::new()
+        //                             }
+        //                         }
+        //                         _ => {
+        //                             eprintln!( "Error: expected type qualification!" );
+        //                             String::new()
+        //                         }
+        //                     },
+        //                     None => {
+        //                         eprintln!( "Error: run out of tokens!" );
+        //                         String::new()
+        //                     }
+        //                 };
+
+        //                 let value = match tokens.next() {
+        //                     Some( token ) => match &token.kind {
+        //                         TokenKind::Equals => match tokens.next() {
+        //                             Some( initializer ) => match &initializer.kind {
+        //                                 TokenKind::Literal{ kind } => match kind {
+        //                                     LiteralKind::Int{ base, value } => {
+        //                                         typ = "int".to_string();
+        //                                         Some( Value::Integer( value.clone(), base.clone() ) )
+        //                                     }
+        //                                 },
+        //                                 _ => {
+        //                                     eprintln!( "Error: expected literal or expression!" );
+        //                                     None
+        //                                 }
+        //                             }
+        //                             None => {
+        //                                 eprintln!( "Error: run out of tokens!" );
+        //                                 None
+        //                             },
+        //                         },
+        //                         _ => {
+        //                             eprintln!( "Error: expected equals" );
+        //                             None
+        //                         },
+        //                     },
+        //                     None => {
+        //                         eprintln!( "Error: run out of tokens!" );
+        //                         None
+        //                     },
+        //                 };
+
+        //                 statements.push( Node::Variable{ mutability, typ, name, value } );
+        //             }
+        //             _ => unreachable!(),
+        //         }
+        //     }
+        // }
 
 //         return Self{ statements };
 //     }
@@ -411,10 +408,10 @@ fn main() -> ExitCode {
     let mut args: Vec<String> = env::args().collect();
 
     // to quickly debug
-    args.push( "./examples/main.blz".to_string() );
+    // args.push( "src/main.rs".to_string() );
     // args.push( "-h".to_string() );
     // args.push( "-v".to_string() );
-    // args.push( "./examples/main.blz".to_string() );
+    args.push( "./examples/main.blz".to_string() );
     // args.push( "-s".to_string() );
 
     if args.len() < 2 {
@@ -472,10 +469,10 @@ fn main() -> ExitCode {
         },
     };
 
-    let tokens = Lexer::parse( source_file_path, source_file );
+    let lexer = Lexer::parse( source_file_path, source_file );
     // let ast = AST::parse( &tokens );
 
-    println!( "{:?}\n", tokens );
+    // println!( "{:?}", lexer );
     // println!( "{:?}", ast );
     return ExitCode::SUCCESS;
 }
